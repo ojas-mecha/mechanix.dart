379a8d557e2836f9144c80cbc77192046ffd73f9
ojas-mecha <ojasc@ext.mechasystems.com>
2026-08-01 21:22:56 +0530

    refactor(package): consolidate public API into single mechanix_widgets barrel export

--- flutter/widgets/lib/mechanix.dart
+++ flutter/widgets/lib/mechanix.dart
@@ -1,12 +1,4 @@
-// You have generated a new plugin project without specifying the `--platforms`
-// flag. A plugin project with no platform support was generated. To add a
-// platform, run `flutter create -t plugin --platforms <platforms> .` under the
-// same directory. You can also find a detailed instruction on how to add
-// platforms in the `pubspec.yaml` at
-// https://flutter.dev/to/pubspec-plugin-platforms.
+/// Backward-compatible export for Mechanix UI library.
+library mechanix;
 
-library;
-
-export 'extension.dart';
-export 'theme.dart';
-export 'widgets.dart';
+export 'mechanix_widgets.dart';
---
+++ flutter/widgets/lib/mechanix_widgets.dart
@@ -0,0 +1,86 @@
+/// Mechanix UI - A collection of common and reusable Flutter widgets designed
+/// specifically for building Mechanix Apps with ease.
+library mechanix_widgets;
+
+// Constants
+export 'src/constants/mechanix_constants.dart';
+export 'src/constants/package_constants.dart';
+
+// Extensions
+export 'src/extensions/border.dart';
+export 'src/extensions/build_context.dart';
+export 'src/extensions/color.dart';
+export 'src/extensions/edge_insets.dart';
+export 'src/extensions/font_size.dart';
+export 'src/extensions/icon_size.dart';
+export 'src/extensions/radius.dart';
+export 'src/extensions/select_label.dart';
+export 'src/extensions/theme_extension.dart';
+
+// Icons
+export 'src/icons/mechanix_icon_images.dart';
+
+// Theme
+export 'src/theme/color_setting.dart';
+export 'src/theme/colors.dart';
+export 'src/theme/mechanix_theme.dart';
+export 'src/theme/theme_data.dart';
+export 'src/theme/touch_splash_factory.dart';
+export 'src/theme/variants.dart';
+
+// Types
+export 'src/types/theme_colors.dart';
+
+// Components & Widgets
+export 'src/widgets/bottom_bar/bottom_bar_button_type.dart';
+export 'src/widgets/bottom_bar/mechanix_bottom_bar.dart';
+export 'src/widgets/bottom_bar/mechanix_bottom_bar_theme.dart';
+export 'src/widgets/bottom_sheet_modals/mechanix_bottom_sheet.dart';
+export 'src/widgets/bottom_sheet_modals/mechanix_bottom_sheet_theme.dart';
+export 'src/widgets/divider/mechanix_menu_divider.dart';
+export 'src/widgets/filled_button/mechanix_filled_button.dart';
+export 'src/widgets/filled_button/mechanix_filled_button_theme.dart';
+export 'src/widgets/floating_action_bar/mechanix_floating_action_bar.dart';
+export 'src/widgets/floating_action_bar/mechanix_floating_action_bar_theme.dart';
+export 'src/widgets/icon_widget.dart';
+export 'src/widgets/list_items/mechanix_simple_list.dart';
+export 'src/widgets/list_items/mechanix_simple_list_theme.dart';
+export 'src/widgets/list_items/simple_list_items_type.dart';
+export 'src/widgets/mechanix_alert_dialog.dart';
+export 'src/widgets/mechanix_bottom_alert.dart';
+export 'src/widgets/mechanix_check_box.dart';
+export 'src/widgets/mechanix_elevated_button.dart';
+export 'src/widgets/mechanix_outline_button.dart';
+export 'src/widgets/mechanix_text_button.dart';
+export 'src/widgets/menu/constants/menu_selection_type.dart';
+export 'src/widgets/menu/mechanix_menu.dart';
+export 'src/widgets/menu/mechanix_menu_theme.dart';
+export 'src/widgets/menu/models/mechanix_menu_item.dart';
+export 'src/widgets/menu/models/menu_position.dart';
+export 'src/widgets/navigation_bar/mechanix_navigation_bar.dart';
+export 'src/widgets/navigation_bar/mechanix_navigation_bar_theme.dart';
+export 'src/widgets/notification/mechanix_notification.dart';
+export 'src/widgets/notification/mechanix_notification_theme.dart';
+export 'src/widgets/notification/notification_type.dart';
+export 'src/widgets/pressable_list/mechanix_pressable_list.dart';
+export 'src/widgets/pressable_list/mechanix_pressable_list_theme.dart';
+export 'src/widgets/search_bar/mechanix_search_bar.dart';
+export 'src/widgets/search_bar/mechanix_search_bar_theme.dart';
+export 'src/widgets/section_list/mechanix_section_list.dart';
+export 'src/widgets/section_list/mechanix_section_list_theme.dart';
+export 'src/widgets/section_list/section_list_items_type.dart';
+export 'src/widgets/select/mechanix_select.dart';
+export 'src/widgets/select/mechanix_select_theme.dart';
+export 'src/widgets/select/select_type.dart';
+export 'src/widgets/slider/mechanix_slider.dart';
+export 'src/widgets/slider/mechanix_slider_theme.dart';
+export 'src/widgets/switch/mechanix_switch.dart';
+export 'src/widgets/switch/mechanix_switch_theme.dart';
+export 'src/widgets/text_input/mechanix_text_input.dart';
+export 'src/widgets/text_input/mechanix_text_input_theme.dart';
+export 'src/widgets/theme/theme_toggle_button.dart';
+export 'src/widgets/theme/variant_picker.dart';
+export 'src/widgets/wheel_scroll/mechanix_wheel_scroll.dart';
+export 'src/widgets/wheel_scroll/mechanix_wheel_scroll_theme.dart';
+export 'src/widgets/wheel_scroll/wheel_scroll_options_type.dart';
+export 'src/widgets/wing/mechanix_wing.dart';
